803d62ea832e9f77c979f652237d9cc282cc4710
martinpodmanicky <mpodmanicky34@gmail.com>
2024-05-09 14:06:41 +0200

    created and edited models and migrations for db setup

---
+++ sforzando/database/migrations/2024_05_09_120210_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->id('user_id');
+            $table->string('product_name');
+            $table->timestamp('date');
+            $table->decimal('price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ sforzando/database/migrations/2024_05_09_120233_create_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('country');
+             // tooto musi byt enum, este musim pozriet ze jako
+             $table->string('city');
+             $table->string('street');
+             $table->string('postal_code');
+             $table->id('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
