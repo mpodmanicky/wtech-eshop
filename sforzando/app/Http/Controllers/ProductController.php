370897fcb7ff65d654ed8f093ba53c8abf19f94b
martinpodmanicky <mpodmanicky34@gmail.com>
2024-05-10 16:28:07 +0200

    updating and pagination

--- sforzando/app/Http/Controllers/ProductController.php
+++ sforzando/app/Http/Controllers/ProductController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Products;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     // Products controller for routing and displaying products
-    public function products(Request $request) {
+    public function index(Request $request) {
+
         $category = $request->input('category');
 
-        $products = Products::where('category', $category)->get();
+        $products = Product::where('category', $category)->get();
 
+        // pagination, ak chcem vsetky produkty, tak paginate(all)
+        $products = Product::paginate(12);
         return view('products');
     }
 
-    public function index() {
-        return view('products');
-    }
+    
 }
